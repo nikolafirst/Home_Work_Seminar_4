af6c5b853c2fa0635ed09878b1fe2b913fc5da3d
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-21 11:52:54 +0300

    solved Example_27

--- Example_27/Program.cs
+++ Example_27/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите целое число: ");
+int Number = int.Parse(Console.ReadLine());
+
+
+int SumAllDigit(int Number)
+{
+    int result = 0;
+    while (Number > 0)
+    {
+        result += Number % 10;
+        Number = Number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма всех чисел в цифре {Number} = {SumAllDigit(Number)}");
