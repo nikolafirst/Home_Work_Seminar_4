55da1a57d21aed3333b2cdd1a6ca9ebf6e922c3f
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-22 09:20:17 +0300

    solved example_25

--- Example_25/Program.cs
+++ Example_25/Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Задача 25:
+//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 
+Console.Write("Введите первое число: ");
+double FirsNumber = double.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double SecondNumber = double.Parse(Console.ReadLine());
+
+
+
+double result = Math.Pow(FirsNumber, SecondNumber);
+Console.WriteLine($"Ответ:" + (result));
